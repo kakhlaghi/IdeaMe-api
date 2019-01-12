4ee2b79948ce0cafdba26783ae8c97e4a53f4948
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-01-11 17:44:40 -0800

    update for post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,19 +1,24 @@
 class Api::V1::PostsController < ApplicationController
     def index
-        @posts = Post.all
+        @posts = Post.order("created_at DESC")
         render json: @posts
     end
 
     def create
-
+        @post = Post.new(post_params)
+        render json: @post
     end
 
-
+    def update
+        @post = Post.find(params[:id])
+        @post.update_attributes(post_params)
+        render json: @post
+    end
 
     private
 
        def post_params
-
+            params_require(:post).permit(:title, :body)
        end
 
 end
